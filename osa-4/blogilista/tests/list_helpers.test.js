505d039730fb4e516f511933c5eb2df4318b99ea
Niko Uusivuori <800057274@omnia.fi>
2026-01-19 12:21:49 +0200

    tests

--- osa-4/blogilista/tests/list_helpers.test.js
+++ osa-4/blogilista/tests/list_helpers.test.js
@@ -3,7 +3,92 @@ const assert = require('node:assert');
 const listHelper = require('../utils/list_helper');
 
 test('dummy returns one', () => {
-    const blogs = [];
-    const result = listHelper.dummy(blogs);
-    assert.strictEqual(result, 1);
+  const result = listHelper.dummy([]);
+  assert.strictEqual(result, 1);
+});
+
+describe('total likes', () => {
+  let blogList = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(blogList);
+    assert.strictEqual(result, 5);
+  });
+  test('when list has nothing', () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+  test('when list has multiple blogs', () => {
+    blogList.push({
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Something',
+      author: 'Someone',
+      url: 'http://google.com',
+      likes: 8,
+      __v: 0
+    });
+    const result = listHelper.totalLikes(blogList);
+    assert.strictEqual(result, 13);
+  });
+});
+
+describe('favourite blog', () => {
+  test('receives empty list', () => {
+    const result = listHelper.favouriteBlog([]);
+    assert.deepStrictEqual(result, null);
+  });
+
+  test('picks blog out of list with one blog', () => {
+    const blogList = [{
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }];
+
+    const result = listHelper.favouriteBlog(blogList);
+    assert.deepStrictEqual(result, blogList[0]);
+  });
+
+  test('picks correct blog out of multiple', () => {
+    const blogList = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f9',
+        title: 'Something',
+        author: 'Someone',
+        url: 'http://google.com',
+        likes: 8,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f7',
+        title: 'Something else',
+        author: 'Someone else',
+        url: 'http://github.com',
+        likes: 8,
+        __v: 0
+      }
+    ];
+    const result = listHelper.favouriteBlog(blogList);
+    assert.deepStrictEqual(result, blogList[1]);
+  });
 });
--- osa-4/blogilista/utils/list_helper.js
+++ osa-4/blogilista/utils/list_helper.js
@@ -1,5 +1,29 @@
+// const blogSchema = mongoose.Schema({
+//     title: String,
+//     author: String,
+//     url: String,
+//     likes: Number,
+// });
+
+const blog = require("../models/blog");
+
 function dummy(blogs) {
     return 1;
 }
 
-module.exports = { dummy };
+function totalLikes(blogs) {
+    return blogs.reduce((sum, blog) => {
+        return sum + blog.likes;
+    }, 0);
+}
+
+function favouriteBlog(blogs) {
+    if (!blogs || blogs.length === 0) return null;
+    let favorite = blogs[0];
+    for (const blog of blogs) {
+        if (favorite.likes < blog.likes) favorite = blog;
+    }
+    return favorite;
+}
+
+module.exports = { dummy, totalLikes, favouriteBlog };
