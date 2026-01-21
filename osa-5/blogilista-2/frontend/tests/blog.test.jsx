b7f3df06f87641ed00392e1cb46a8eea886557fe
Niko Uusivuori <800057274@omnia.fi>
2026-01-21 09:05:39 +0200

    view button test for blog component

--- osa-5/blogilista-2/frontend/tests/blog.test.jsx
+++ osa-5/blogilista-2/frontend/tests/blog.test.jsx
@@ -1,7 +1,9 @@
-import { afterEach } from 'vitest';
+import { afterEach, expect } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import { render, screen } from '@testing-library/react'
+
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 
 import Blog from '../src/components/Blog';
 
@@ -9,22 +11,47 @@ afterEach(() => {
     cleanup();
 });
 
-test('renders blog title and author', () => {
-    const blogData = {
-        id: "1",
-        title: "Test Blog",
-        author: "Some Author",
-        url: "https://google.com",
-        likes: 5,
-        user: {
-            name: "admin",
-            username: "admin",
-            id: "696e88273e8f53465062b08b"
-        }
-    };
+const blogData = {
+    id: "1",
+    title: "Test Blog",
+    author: "Some Author",
+    url: "https://google.com",
+    likes: 5,
+    user: {
+        name: "admin",
+        username: "admin",
+        id: "696e88273e8f53465062b08b"
+    }
+};
 
+
+test('renders blog title and author', () => {
     render(<Blog blog={blogData} />);
     
     const expectedText = `${blogData.title} - ${blogData.author}`;
     expect(screen.getByText(expectedText)).toBeInTheDocument();
+});
+
+test('shows url, likes, creator only when view button is pressed', async () => {
+    render(<Blog blog={blogData} />);
+
+    const expectedTexts = [
+        `Likes: ${blogData.likes}`,
+        `Url: ${blogData.url}`,
+        `Added by: ${blogData.user.name}`
+    ];
+
+    for (const expectedText of expectedTexts) {
+        const element = screen.getByText(expectedText);
+        expect(element).not.toBeVisible();
+    }
+
+    const user = userEvent.setup();
+    const button = screen.getByText('View');
+    await user.click(button);
+
+    for (const expectedText of expectedTexts) {
+        const element = screen.getByText(expectedText);
+        expect(element).toBeVisible();
+    }
 });
